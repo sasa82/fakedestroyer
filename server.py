a4acbfd4d9362fa2dcc0e705ab9abc03b6a87d41
sasa <bort8282@mail.ru>
2020-11-28 21:25:36 +0800

    paths

--- server.py
+++ server.py
@@ -6,31 +6,24 @@ import os
 from Cheetah.Template import Template
 
 
-#cherrypy.server.socket_host = '0.0.0.0'
-#cherrypy.config.update({'server.socket_port': 80})
-#css_path = os.path.abspath("fakedestroyer-front/assets/style/style.css")
-
-
 class FakeDestroyer(object):
 	@cherrypy.expose
 	def index(self, platform=''):
-		templ_path = os.path.abspath("fakedestroyer-front/index.html")
-#		html_template = Template(file=templ_path)
-#		html_template.css_scripts=[css_path]
-		html_template = Template(file='fakedestroyer-front/index.html')
+		html_template = Template(file='templates/index.html')
 		html_template.css_scripts=['static/style/style.css']
 		return str(html_template)
-		#return open(path)
 		
 	@cherrypy.expose
 	def check(self):
-		path = os.path.abspath("fakedestroyer-front/check.html")
-		return open(path)
+		html_template = Template(file='templates/check.html')
+		html_template.css_scripts=['static/style/style.css']
+		return str(html_template)
 		
 	@cherrypy.expose
 	def about(self):
-		path = os.path.abspath("fakedestroyer-front/about.html")
-		return open(path)
+		html_template = Template(file='templates/about.html')
+		html_template.css_scripts=['static/style/style.css']
+		return str(html_template)
 				
 	@cherrypy.expose
 	def api_check(self):
