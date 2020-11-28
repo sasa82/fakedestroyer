d6de5e84d6e73b5baaa376ef43c745ff65a9ad4d
sasa <bort8282@mail.ru>
2020-11-28 21:13:10 +0800

    paths

--- server.py
+++ server.py
@@ -6,17 +6,19 @@ import os
 from Cheetah.Template import Template
 
 
-cherrypy.server.socket_host = '0.0.0.0'
-cherrypy.config.update({'server.socket_port': 80})
-css_path = os.path.abspath("fakedestroyer-front/assets/style/style.css")
+#cherrypy.server.socket_host = '0.0.0.0'
+#cherrypy.config.update({'server.socket_port': 80})
+#css_path = os.path.abspath("fakedestroyer-front/assets/style/style.css")
 
 
 class FakeDestroyer(object):
 	@cherrypy.expose
 	def index(self, platform=''):
 		templ_path = os.path.abspath("fakedestroyer-front/index.html")
-		html_template = Template(file=templ_path)
-		html_template.css_scripts=[css_path]
+#		html_template = Template(file=templ_path)
+#		html_template.css_scripts=[css_path]
+		html_template = Template(file='fakedestroyer-front/index.html')
+		html_template.css_scripts=['fakedestroyer-front/assets/style/style.css']
 		return str(html_template)
 		#return open(path)
 		
@@ -33,6 +35,28 @@ class FakeDestroyer(object):
 	@cherrypy.expose
 	def api_check(self):
 		print("inside api request")
+
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
+config = {
+'global': {
+	'environment': 'production',
+	'log.screen': True,
+	'server.socket_host': '0.0.0.0',
+	'server.socket_port': 80,
+	'engine.autoreload_on': True,
+	'log.error_file': os.path.join(current_dir, 'errors.log'),
+	'log.access_file': os.path.join(current_dir, 'access.log'),
+	},
+	'/':{
+	'tools.staticdir.root' : current_dir,
+	},
+	'/static':{
+	'tools.staticdir.on' : True,
+	'tools.staticdir.dir' : 'static',
+	},
+}
 
 
 if __name__ == '__main__':
