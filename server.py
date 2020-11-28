2172bf9dcdef3e04a44acb6bb8d0264a3e9c16d4
sasa <bort8282@mail.ru>
2020-11-28 21:08:10 +0800

    paths

--- server.py
+++ server.py
@@ -3,16 +3,22 @@ import string
 
 import cherrypy
 import os
+from Cheetah.Template import Template
 
 
 cherrypy.server.socket_host = '0.0.0.0'
 cherrypy.config.update({'server.socket_port': 80})
+css_path = os.path.abspath("fakedestroyer-front/assets/style/style.css")
+
 
 class FakeDestroyer(object):
 	@cherrypy.expose
 	def index(self, platform=''):
-		path = os.path.abspath("fakedestroyer-front/index.html")
-		return open(path)
+		templ_path = os.path.abspath("fakedestroyer-front/index.html")
+		html_template = Template(file=templ_path)
+		html_template.css_scripts=[css_path]
+		return str(html_template)
+		#return open(path)
 		
 	@cherrypy.expose
 	def check(self):
